19fadf1fff2b18ba743771a25e688a5106923af3
rounaksalim95 <rounaksalim@gmail.com>
2022-11-20 11:43:37 -0800

    Update IP address for deployment

--- frontend/src/webConfig.js
+++ frontend/src/webConfig.js
@@ -1,3 +1,3 @@
 // const backendServer = "http://localhost:5000";
-const backendServer = "http://54.183.6.204:5000";
+const backendServer = "http://18.144.165.108:5000";
 export default backendServer;
